eeabed4ca251bd3082ac77b61f8d16bb3d82697c
sarvenaz-vsl <sarvenaz.vsl@gmail.com>
2021-05-16 20:37:28 +0430

    adding picture to sidebar

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let toggle = document.querySelector('.toggle');
 let navigation = document.querySelector('.navigation');
 let main = document.querySelector('.main');
+let user = document.querySelector('.sidebar-user');
 
 toggle.addEventListener('click', toggleMenu);
 
@@ -8,4 +9,5 @@ function toggleMenu() {
     toggle.classList.toggle('active');
     navigation.classList.toggle('active');
     main.classList.toggle('active');
+    user.classList.toggle('active');
 }
